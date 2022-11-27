fac98f88be5e2ff422934e8dffd2422f827bf26f
Arindam Barman <arindambarmangithub@gmail.com>
2022-11-27 18:38:12 +0530

    simple sign up and sign in

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,14 +1,18 @@
-import * as React from 'react';
+import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import Sidebar from 'app/components/Sidebar';
+import { Outlet } from 'react-router-dom';
 
 export function HomePage() {
   return (
     <>
       <Helmet>
         <title>HomePage</title>
-        <meta name="description" content="A Boilerplate application homepage" />
+        <meta name="description" content="Well hello" />
       </Helmet>
-      <span>My HomePage</span>
+      <Sidebar>
+        <Outlet />
+      </Sidebar>
     </>
   );
 }
