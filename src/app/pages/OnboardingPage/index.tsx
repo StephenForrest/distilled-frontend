6c1d732c1820992d9727cbc8760cc656c1d3b28e
Semen Samosiuk <samasiuk1986@gmail.com>
2023-01-24 16:18:06 +0300

    fix: style and add Stepper component

--- src/app/pages/OnboardingPage/index.tsx
+++ src/app/pages/OnboardingPage/index.tsx
@@ -1,26 +1,22 @@
-// import {
-//   Button,
-//   ButtonGroup,
-//   Stepper,
-//   StepperCompleted,
-//   StepperStep,
-// } from '@saas-ui/react';
-// import {
-//   Spacer,
-//   Box,
-//   Heading,
-//   FormControl,
-//   FormLabel,
-//   Input,
-//   Select,
-//   VStack,
-//   Grid,
-//   Text,
-// } from '@chakra-ui/react';
-// import { Helmet } from 'react-helmet-async';
+import { Helmet } from 'react-helmet-async';
 // import { useForm } from 'react-hook-form';
-// import Lottie from 'react-lottie';
-// import * as animationData from 'app/jsons/checkmark.json';
+import Lottie from 'react-lottie';
+import * as animationData from 'app/jsons/checkmark.json';
+import {
+  Button,
+  Flex,
+  Heading,
+  Box,
+  Spacer,
+  Text,
+  Grid,
+  VStack,
+  FormControl,
+  FormLabel,
+  Input,
+  Select,
+  AspectRatio,
+} from '@chakra-ui/react';
 import * as React from 'react';
 
 // declare global {
@@ -42,8 +38,187 @@ import * as React from 'react';
 //   numOfEmployees: string;
 // }
 
+import { Step, Steps, useSteps } from 'chakra-ui-steps';
+import { FiClipboard, FiDollarSign, FiUser } from 'react-icons/fi';
+
+const steps = [
+  {
+    label: 'Information',
+    icon: FiUser,
+    children: (
+      <Box width={{ base: '100%' }} mb="15px">
+        <FormControl pt="6">
+          <FormLabel htmlFor="first-name">First Name</FormLabel>
+          <Input id="first-name" isRequired name="firstName" type="text" />
+        </FormControl>
+
+        <FormControl mt={4}>
+          <FormLabel htmlFor="last-name">Last Name</FormLabel>
+          <Input id="last-name" isRequired name="lastName" type="text" />
+        </FormControl>
+
+        <FormControl mt={4}>
+          <FormLabel htmlFor="company">Company</FormLabel>
+          <Input id="company" isRequired name="company" type="text" />
+        </FormControl>
+
+        <FormControl mt={4}>
+          <FormLabel htmlFor="role">Role</FormLabel>
+          <Select id="role" isRequired name="role">
+            <option value="">Select a role</option>
+            <option value="developer">Developer Advocate</option>
+            <option value="designer">Community Manager</option>
+            <option value="product-manager">Leadership</option>
+            <option value="product-manager">Purchasing</option>
+            <option value="product-manager">Project Manager</option>
+          </Select>
+        </FormControl>
+
+        <FormControl mt={4}>
+          <FormLabel htmlFor="number-of-employees">
+            Number of Employees!
+          </FormLabel>
+          <Select isRequired id="number-of-employees" name="numOfEmployees">
+            <option value="">Select the number of employees</option>
+            <option value="1-10">1-10</option>
+            <option value="11-50">11-50</option>
+            <option value="51-200">51-200</option>
+            <option value="201-500">201-500</option>
+            <option value="500+">500+</option>
+          </Select>
+        </FormControl>
+      </Box>
+    ),
+  },
+  {
+    label: 'Pricing',
+    icon: FiDollarSign,
+    children: (
+      <>
+        <Helmet>
+          <title>Pricing</title>
+          <meta name="description" content="Pricing" />
+          <script
+            async
+            src="https://js.stripe.com/v3/pricing-table.js"
+          ></script>
+        </Helmet>
+        <Box width={{ base: '100%' }} height={{ base: '100%' }} my="15px">
+          <script
+            async
+            src="https://js.stripe.com/v3/pricing-table.js"
+          ></script>
+          <stripe-pricing-table
+            pricing-table-id="prctbl_1MPnqqFCaczULzzDyLOoHu2h"
+            publishable-key="pk_live_51M9DryFCaczULzzDhcLdJx0EA8pG3JuLAlqdRc4PkW1Nj4p9jD81Ez46qYroTdU2CYFclOf1kAkctGqHaPVwvCYa0064n7XlT2"
+          ></stripe-pricing-table>
+        </Box>
+      </>
+    ),
+  },
+  {
+    label: 'Demo',
+    icon: FiClipboard,
+    children: (
+      <Grid
+        templateColumns="1fr"
+        alignItems="center"
+        justifyContent="center"
+        mt="15px"
+      >
+        <VStack align="center" justify="center">
+          <AspectRatio
+            width={{ base: '320px', sm: '420px', md: '520px', lg: '720px' }}
+            height="100%"
+          >
+            <iframe
+              title="demo"
+              src="https://demo.arcade.software/1KfsiicMWjdsE4m2SbNe?embed"
+            />
+          </AspectRatio>
+        </VStack>
+      </Grid>
+    ),
+  },
+];
+
 export function Onboarding() {
-  return <div>Onboarding</div>;
+  const { nextStep, prevStep, activeStep } = useSteps({
+    initialStep: 0,
+  });
+
+  return (
+    <Box display="flex" flexDir="column" justifyContent="center">
+      <Heading textAlign="center" pt={20} pb={10} mb={4}>
+        Let's get started! 💪
+      </Heading>
+      <Box display="flex" justifyContent="center">
+        <Flex
+          alignItems="center"
+          flexDir="column"
+          width={{ base: '100%', md: '50%' }}
+          px="15px"
+        >
+          <Steps activeStep={activeStep} borderColor="brand.500">
+            {steps.map(({ label, icon, children }, index) => (
+              <Step label={label} icon={icon} key={index}>
+                {children}
+              </Step>
+            ))}
+          </Steps>
+          {activeStep === steps.length ? (
+            <Flex px={4} py={4} width="100%" flexDirection="column">
+              <Heading fontSize="xl" textAlign="center">
+                <Box mt="15px">
+                  <Heading>Congratulations!</Heading>
+                  <Text>You have completed the onboarding process.</Text>
+                </Box>
+                <Lottie
+                  options={{
+                    animationData: animationData, // this is the json object of the lottie file
+                    loop: false,
+                    autoplay: true,
+                  }}
+                  height={200}
+                  width={200}
+                />
+              </Heading>
+              <Button
+                mx="auto"
+                mt={6}
+                size="sm"
+                colorScheme="brand"
+                onClick={() => {
+                  alert(`go to homePage`);
+                }}
+              >
+                Start
+              </Button>
+            </Flex>
+          ) : (
+            <Flex width="100%" justify="flex-end">
+              <Button
+                color="#ffffff"
+                bg="#6d30cf"
+                isDisabled={activeStep === 0}
+                mr={4}
+                onClick={prevStep}
+                size="sm"
+                variant="ghost"
+                _hover={{ bg: '#5727a5' }}
+              >
+                Back
+              </Button>
+              <Spacer />
+              <Button size="sm" onClick={nextStep} colorScheme="brand">
+                {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+              </Button>
+            </Flex>
+          )}
+        </Flex>
+      </Box>
+    </Box>
+  );
 }
 // export function Onboarding() {
 //   const [step, setStep] = React.useState(0);
