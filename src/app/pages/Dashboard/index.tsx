eb5c761795c774e1259e70aee0444638afa2bf83
James <james.bohrman@fermyon.com>
2023-01-26 00:47:37 -0500

    Start dashboard

--- src/app/pages/Dashboard/index.tsx
+++ src/app/pages/Dashboard/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import WelcomeToDistilled from 'app/components/WelcomeToDistilled';
 import CreateNewPlanModal from 'app/components/CreateNewPlanModal';
-import { Grid, VStack } from '@chakra-ui/react';
+import { Divider, Grid, Heading, VStack } from '@chakra-ui/react';
 
 export function Page() {
   const [isNewPlanModal, setIsNewPlanModal] = useState<boolean>(false);
@@ -15,14 +15,13 @@ export function Page() {
       <Grid templateColumns="1fr" alignItems="center" justifyContent="center">
         <VStack align="center" justify="center">
           <WelcomeToDistilled />
-          <iframe
-            title="demo"
-            src="https://demo.arcade.software/1KfsiicMWjdsE4m2SbNe?embed"
-            width="720px"
-            height="480px"
-          />
         </VStack>
       </Grid>
+      <Divider justifyContent="center" width="870px" />
+      <Heading justifyContent="left" size="md" pl="6" mt="6">
+        Get the most from Distilled
+      </Heading>
+
       <CreateNewPlanModal
         isOpen={isNewPlanModal}
         onClose={() => setIsNewPlanModal(false)}
