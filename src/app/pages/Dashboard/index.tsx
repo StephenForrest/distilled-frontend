abcaddede2be042ae18c5b2e9e41729dbffd287b
Semen Samosiuk <samasiuk1986@gmail.com>
2023-01-26 17:41:23 +0300

    feat: dashboard-content

--- src/app/pages/Dashboard/index.tsx
+++ src/app/pages/Dashboard/index.tsx
@@ -2,7 +2,20 @@ import React, { useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import WelcomeToDistilled from 'app/components/WelcomeToDistilled';
 import CreateNewPlanModal from 'app/components/CreateNewPlanModal';
-import { Divider, Grid, Heading, VStack } from '@chakra-ui/react';
+import AppIcon from 'app/components/AppIcons';
+import {
+  Box,
+  Divider,
+  Flex,
+  Grid,
+  Heading,
+  GridItem,
+  Text,
+  VStack,
+  Image,
+} from '@chakra-ui/react';
+
+import gallery from '../../../assets/web-gallery.png';
 
 export function Page() {
   const [isNewPlanModal, setIsNewPlanModal] = useState<boolean>(false);
@@ -17,10 +30,123 @@ export function Page() {
           <WelcomeToDistilled />
         </VStack>
       </Grid>
-      <Divider justifyContent="center" width="870px" />
+      <Box w="100%" px={20}>
+        <Divider justifyContent="center" width="100%" />
+        <Heading justifyContent="left" size="md" pl="6" mt="6">
+          Get the most from Distilled
+        </Heading>
+      </Box>
+      <Grid
+        templateColumns="repeat(2, 1fr)"
+        gap={6}
+        alignItems="center"
+        justifyContent="center"
+        px={20}
+      >
+        <GridItem w="100%" p={5}>
+          <Flex display={{ base: 'block', xl: 'flex' }}>
+            <Box
+              p={2}
+              display="flex"
+              w="55px"
+              h="55px"
+              alignItems="center"
+              justifyContent="center"
+              bgColor="rgb(237,242,247)"
+              borderRadius="15px"
+            >
+              <Image as={AppIcon['slack']} w="40px" h="40px" />
+            </Box>
+            <Box ml="10px">
+              <Text as="b">Request an invite</Text>
+              <Text>
+                Join a shared Slack Connect channel with the Distilled team
+              </Text>
+            </Box>
+          </Flex>
+        </GridItem>
+        <GridItem w="100%">
+          <Flex>
+            <Box
+              p={2}
+              display="flex"
+              h="60px"
+              w="60px"
+              alignItems="center"
+              justifyContent="center"
+              bgColor="rgb(237,242,247)"
+              borderRadius="15px"
+            >
+              <Image src={gallery} w="100%" h="100%" alt="gallery" />
+            </Box>
+            <Box ml="10px">
+              <Text as="b">Invite your teammates</Text>
+              <Text>Invite your team to collaboratively build strategy</Text>
+            </Box>
+          </Flex>
+        </GridItem>
+      </Grid>
+      <Box w="100%" px={20}>
+        <Divider justifyContent="center" width="100%" />
+        <Heading justifyContent="left" size="md" pl="6" mt="6">
+          Learn about OKRs
+        </Heading>
+      </Box>
+      <Grid
+        templateColumns="repeat(2, 1fr)"
+        gap={6}
+        alignItems="center"
+        justifyContent="center"
+        px={20}
+      >
+        <GridItem w="100%" py={5} pl={5}>
+          <Flex display={{ base: 'block', xl: 'flex' }}>
+            <Box
+              p={2}
+              display="flex"
+              h="60px"
+              w="60px"
+              alignItems="center"
+              justifyContent="center"
+              bgColor="rgb(237,242,247)"
+              borderRadius="15px"
+            >
+              <Image src={gallery} w="100%" h="100%" alt="gallery" />
+            </Box>
+            <Box ml="10px">
+              <Text as="b">Gallery (Coming soon)</Text>
+              <Text>Access over 500+ OKR resources and templates</Text>
+            </Box>
+          </Flex>
+        </GridItem>
+        <GridItem w="100%">
+          <Flex>
+            <Box
+              p={2}
+              display="flex"
+              h="55px"
+              alignItems="center"
+              justifyContent="center"
+              bgColor="rgb(237,242,247)"
+              borderRadius="15px"
+            >
+              <Image as={AppIcon['slack']} w="40px" h="40px" />
+            </Box>
+            <Box ml="10px">
+              <Text as="b">OKR Academy (Coming soon)</Text>
+              <Text>
+                Learn strategy development techniques from experts in various
+                niche
+              </Text>
+            </Box>
+          </Flex>
+        </GridItem>
+      </Grid>
+
+      {/* <Divider justifyContent="center" width="500px" />
       <Heading justifyContent="left" size="md" pl="6" mt="6">
-        Get the most from Distilled
-      </Heading>
+        Get the most from
+      </Heading> */}
 
       <CreateNewPlanModal
         isOpen={isNewPlanModal}
