9c309dcd2b1471144933c75fe4676224ec09dcc9
James <james.bohrman@fermyon.com>
2023-02-14 04:50:58 -0500

    tasks

--- src/app/pages/SettingsPage/index.tsx
+++ src/app/pages/SettingsPage/index.tsx
@@ -10,7 +10,7 @@ import {
 } from '@chakra-ui/react';
 import UserSettings from './UserSettings';
 import WorkspaceSettings from './WorkspaceSettings';
-import ApiKeyForm from './ApiKeyForm';
+import ApiKey from './ApiKeyForm';
 
 export function Settings() {
   return (
@@ -31,7 +31,7 @@ export function Settings() {
             <WorkspaceSettings />
           </TabPanel>
           <TabPanel>
-            <ApiKeyForm />
+            <ApiKey />
           </TabPanel>
         </TabPanels>
       </Tabs>
