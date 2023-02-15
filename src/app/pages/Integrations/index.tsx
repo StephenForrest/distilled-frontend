208b9721039f3ba38ce96d90407af5df6b53b2a7
James <james.bohrman@fermyon.com>
2023-02-15 02:26:22 -0500

    zapier

--- src/app/pages/Integrations/index.tsx
+++ src/app/pages/Integrations/index.tsx
@@ -12,6 +12,8 @@ import {
   ModalBody,
   ModalFooter,
   ModalHeader,
+  LinkBox,
+  LinkOverlay,
 } from '@chakra-ui/react';
 import { useDisclosure } from '@chakra-ui/hooks';
 import PageHeader from 'app/components/PageHeader';
@@ -23,24 +25,28 @@ export const IntegrationsPage = () => {
     <>
       <PageHeader text="Integrations" />
       <HStack pl="5">
-        <Box w="350px" bg="gray.100" h="200px" m="5" borderRadius={4}>
-          <Image
-            src="https://asset.brandfetch.io/idNMs_nMA0/idDC3DWoZ8.svg?updated=1667563370508"
-            mx="auto"
-            alt="Zapier logo"
-            style={{ width: '200px', height: '200px' }}
-            onClick={onOpen}
-          />
-        </Box>
+        <LinkBox onClick={onOpen}>
+          <LinkOverlay href="#">
+            <Box w="350px" bg="gray.100" h="200px" m="5" borderRadius={4}>
+              <Image
+                src="https://asset.brandfetch.io/idNMs_nMA0/idDC3DWoZ8.svg?updated=1667563370508"
+                mx="auto"
+                alt="Zapier logo"
+                style={{ width: '200px', height: '200px' }}
+                onClick={onOpen}
+              />
+            </Box>
+          </LinkOverlay>
+        </LinkBox>
       </HStack>
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal isOpen={isOpen} onClose={onClose} size={'4xl'}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Integrate via Zapier</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
             <zapier-full-experience
-              client-id="QRpZR4B484qiMIhBjreSnjECgKX1VudjON13vY7D"
+              client-id="ZAPIER_CLIENT_ID"
               theme="light"
               app-search-bar-display="show"
             />
